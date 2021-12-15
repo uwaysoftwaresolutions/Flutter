019096a1a4fe151e5c2cb2a0977b7a0758146886
durgesh145 <67770479+durgesh145@users.noreply.github.com>
2021-12-15 14:37:18 +0530

    practice new learn

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        appBar: AppBar(
-          title: Text("My natpu alok,Nissar"),
-          backgroundColor: Colors.blueGrey[900],
-        ),
-        body: Center(
-          child: Image(
-            image:
-            NetworkImage('https://www.w3schools.com/w3css/img_lights.jpg'),
+void main(){
+  runApp(new Application());
+}
+
+class Application extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "Stateless Widget",
+      home: new Scaffold(
+        body: new Container(
+          color: Colors.pink,
+          child: new Container(
+            color: Colors.yellow,
+            margin: const EdgeInsets.all(30.0),
           ),
-        ),
+        )
+       
       ),
-    ),
-  );
+    );
+  }
 }
