3ce20601cc13e22df3ebe0de7c48643da4f51153
durgesh145 <67770479+durgesh145@users.noreply.github.com>
2021-12-16 09:20:04 +0530

    new change

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,26 +1,50 @@
 import 'package:flutter/material.dart';
 
-
 void main() {
-  runApp(new MaterialApp(home: new Application()));
+  runApp(MaterialApp(home: Application()));
 }
 
 class Application extends StatefulWidget {
   @override
-  _ApplicationState createState() => new _ApplicationState();
+  _ApplicationState createState() => _ApplicationState();
 }
 
 class _ApplicationState extends State<Application> {
 
+
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Appbar'),
-        backgroundColor: Colors.green,
-        elevation: 30.0,
-        centerTitle: true,
-        titleSpacing: 70.0,
-        toolbarOpacity: 0.5,
+    return Scaffold(
+      appBar: AppBar(title: Text('Drawer'),),
+      drawer: Drawer(
+        child: ListView(
+              children: <Widget>[
+                UserAccountsDrawerHeader(
+                  accountName: Text('durgesh kumar'),
+                  accountEmail: Text('durgeshemail.com'),
+                  currentAccountPicture: CircleAvatar(
+                    backgroundColor: Colors.black26,
+                    child: Text('M'),
+                  ),
+                  decoration: BoxDecoration(color: Colors.orange),
+                ),
+                ListTile(
+                  title: Text('Page 1'),
+                  trailing: Icon(Icons.arrow_forward),
+                  onTap: ()=> Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context)=>Row("page1"))),
+                ),
+                ListTile(
+                  title: Text('Page 2'),
+                  trailing: Icon(Icons.arrow_forward),
+                  onTap: ()=> Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context)=>Row('page2'))),
+
+                ),
+                ListTile(
+                  title: Text('Close'),
+                  trailing: Icon(Icons.close),
+                  onTap: (){Navigator.pop(context);},
+                ),
+              ],
+            )
       ),
     );
   }
---
+++ my_app/my_id/android/app/src/main/kotlin/com/example/my_id/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.my_id
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
